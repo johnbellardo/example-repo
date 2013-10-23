751622ca0eb37cb1477545382497e4f7b17dec40
Demo <demo@pcp075858pcs.wireless.calpoly.edu>
2013-10-23 09:13:05 -0700

    Add startup output

--- example/example/AppDelegate.m
+++ example/example/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSLog(@"Git is cool!");
     return YES;
 }
 							
